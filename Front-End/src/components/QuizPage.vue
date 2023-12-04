4a3d153f8bbb6ea94cefc6c34e3e4a144024df8d
rakinashrafkhan <rakinashrafkhan@gmail.com>
2023-12-03 19:33:08 -0700

    Questions API integration done

--- Front-End/src/components/QuizPage.vue
+++ Front-End/src/components/QuizPage.vue
@@ -1,21 +1,31 @@
 <template>
-    <div class="quiz-page">
-      <h2>Quiz: {{ categoryName }}</h2>
-      <div v-if="loading" class="loading">Loading questions...</div>
-      <div v-else>
-        <ul class="questions-list">
-          <li v-for="question in questions" :key="question.id" class="question-item">
-            <h3>{{ question.text }}</h3>
-            <ul class="options-list">
-              <li v-for="option in question.options" :key="option.id" class="option-item">
+  <div class="quiz-page container py-5">
+    <h2 class="text-center">Quiz</h2>
+    <div v-if="loading" class="text-center">Loading questions...</div>
+    <div v-else class="mt-4">
+      <div class="card mb-4" v-for="(question, index) in questions" :key="question.id">
+        <div class="card-header">
+          Question {{ index + 1 }}
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">{{ question.text }}</h5>
+          <div class="mb-3">
+            <div class="form-check" v-for="(option, idx) in question.options" :key="idx">
+              <input class="form-check-input" type="radio" :name="'option' + question.id" :id="'option' + question.id + '-' + idx" :value="option.correct">
+              <label class="form-check-label" :for="'option' + question.id + '-' + idx">
                 {{ option.text }}
-              </li>
-            </ul>
-          </li>
-        </ul>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="text-center">
+        <button class="btn btn-primary" @click="submitQuiz">Submit Quiz</button>
       </div>
     </div>
-  </template>
+  </div>
+</template>
+
   
   <script>
   export default {
@@ -33,28 +43,40 @@
       };
     },
     created() {
-      this.fetchQuizQuestions();
+      this.fetchQuestions();
     },
     methods: {
-      fetchQuizQuestions() {
+      fetchQuestions() {
         this.loading = true;
         this.$axios.get(`${process.env.VUE_APP_API_URL}/api/questions/${this.categoryId}`)
           .then(response => {
             this.questions = response.data;
-            this.categoryName = response.data.length > 0 ? response.data[0].category.name : 'Unknown';
+            // this.categoryName = response.data.length > 0 ? response.data[0].category.name : 'Unknown';
             this.loading = false;
           })
           .catch(error => {
             console.error("Error fetching questions:", error);
             this.loading = false;
-            alert("Failed to load questions.");
+            // alert("Failed to load questions.");
           });
       },
+      submitQuiz() {
+      // TODO: Handle quiz submission
+      alert('Quiz submitted!');
+    },
     },
   };
   </script>
   
   <style scoped>
+  .quiz-page {
+  max-width: 800px;
+  margin: auto;
+}
+
+.form-check-label {
+  margin-left: 0.3rem;
+}
   .loading {
     text-align: center;
   }
