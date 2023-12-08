718df06fe6762998ac37ba3d2fb2f054fb0cb95a
Raisa Mehjabin Azni <150555460+raisa-azni@users.noreply.github.com>
2023-12-09 03:02:46 +0600

    QuizMaster agent comments

--- QMSoftware/src/Agents/QuizMaster.java
+++ QMSoftware/src/Agents/QuizMaster.java
@@ -1,3 +1,8 @@
+/*    
+The QuizMaster agent acts as a middle bridge to handle commands sent between the agents. This agent's implementation is more 
+beneficial in real-life scenarios where we may have a large number of agents working on a project.
+*/
+
 package Agents;
 
 import jade.core.AID;
@@ -5,8 +10,13 @@ import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 
+// Main QuizMaster Agent Class, which extends the Agent library.
+
 public class QuizMaster extends Agent {
+    
     private MessageHandler messageHandler;
+
+    // This is the setup() function which holds the 'agent has been started' comment and the implemented behaviours in this agent.
 
     @Override
     protected void setup() {
@@ -16,7 +26,13 @@ public class QuizMaster extends Agent {
         addBehaviour(new ReceiveCategorySelectionBehaviour());
     }
 
-    // Behaviour for initiating the quiz
+    /* 
+    Behaviour for initiating the quiz. 
+    It sends the command 'fetch the available categories from the database' to the 
+    database agent. After getting this command, the database agent will fetch the available categories from the database.
+    This command basically starts the quiz process.
+    */
+    
     private class InitiateQuizBehaviour extends CyclicBehaviour {
         public void action() {
             ACLMessage msg = receive();
@@ -29,7 +45,12 @@ public class QuizMaster extends Agent {
         }
     }
 
-    // New Behaviour for receiving category selection from Interface agent
+    /* 
+       Behaviour for receiving category selection from the interface agent.
+       This behavior receives the 'desired category entered by the user' from the interface agent and sends it to the database
+       agent. The database agent will then receive this category serial number and generate the questions for that particular category.
+    */
+    
     private class ReceiveCategorySelectionBehaviour extends CyclicBehaviour {
         public void action() {
             ACLMessage msg = receive();
@@ -46,6 +67,7 @@ public class QuizMaster extends Agent {
     }
 
     // MessageHandler class to encapsulate message sending logic
+    
     private class MessageHandler {
         private Agent agent;
 
