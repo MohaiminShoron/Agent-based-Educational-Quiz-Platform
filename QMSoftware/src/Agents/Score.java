1dde1146811e8340f82481b54a054cbbbd15cbbf
Raisa Mehjabin Azni <150555460+raisa-azni@users.noreply.github.com>
2023-12-09 03:44:13 +0600

    Score agent comments

--- QMSoftware/src/Agents/Score.java
+++ QMSoftware/src/Agents/Score.java
@@ -1,16 +1,26 @@
+// This is a small agent that handles the printing feature of the computed scores generated from the whole quiz session.
+
 package Agents;
 
 import jade.core.Agent;
 import jade.core.behaviours.SimpleBehaviour;
 import jade.lang.acl.ACLMessage;
 
+// Main Score Agent Class, which extends the Agent library.
+
 public class Score extends Agent {
 
+    // This is the setup() function that holds the 'agent has been started' comment and the implemented behaviours in this agent.
     @Override
     protected void setup() {
         System.out.println(getLocalName() + " is ready.");
         addBehaviour(new PublishScoreBehaviour());
     }
+
+    /*
+      This behaviour receives the computed score from the QuestionInterface agent and prints it for the user to see how much 
+      the user could score.
+    */
 
     private class PublishScoreBehaviour extends SimpleBehaviour {
         private boolean done = false;
