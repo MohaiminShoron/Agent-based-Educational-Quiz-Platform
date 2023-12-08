b1b3702b20601d3ebfba90adf6d3ee1c4c1dec62
Raisa Mehjabin Azni <150555460+raisa-azni@users.noreply.github.com>
2023-12-09 03:26:40 +0600

    Database agent comments

--- QMSoftware/src/Agents/Database.java
+++ QMSoftware/src/Agents/Database.java
@@ -1,3 +1,9 @@
+/*
+   The database handles all the mechanism that will happen on the database
+   1. Retrieving information from the database.
+   2. Saving information from the database for future retrieval.
+*/
+
 package Agents;
 
 import jade.core.AID;
@@ -13,11 +19,14 @@ import java.sql.SQLException;
 
 
 public class Database extends Agent {
+    
     // Database connection parameters
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/seng696";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "pknbgr86A@";
     private Gson gson;
+
+     // This is the setup() function which holds the 'agent has been started' comment and the implemented behaviours in this agent.
 
     @Override
     protected void setup() {
@@ -28,7 +37,48 @@ public class Database extends Agent {
         addBehaviour(new HandleRetrievingQuestionsBehaviour());
     }
 
-    // Behaviour for handling requests from QuizmasterAgent
+    /* 
+       Behaviour to  UserInfo messages
+       This behaviour saves the information received from the user to the database for registration, log-in and authentication
+       purpose. It calls a function called 'storeUserInfoInDatabase(userInfo)' which receives the user information and makes the
+       necessary arrangement to perform this feature.
+    */
+    
+    private class HandleUserInfoBehaviour extends CyclicBehaviour {
+        public void action() {
+            ACLMessage msg = receive();
+            if (msg != null && msg.getOntology().equals("UserInfo")) {
+                UserInfo userInfo = gson.fromJson(msg.getContent(), UserInfo.class);
+                storeUserInfoInDatabase(userInfo);
+            } else {
+                block();
+            }
+        }
+    }
+
+    // The function to store user information to the database according to the database table.
+    
+    private void storeUserInfoInDatabase(UserInfo userInfo) {
+        String sql = "INSERT INTO users (email, password, username) VALUES (?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, userInfo.getEmail());
+            pstmt.setString(2, userInfo.getPassword());
+            pstmt.setString(3, userInfo.getUsername()); // Consider hashing passwords
+
+            pstmt.executeUpdate();
+            System.out.println("\nUser info stored successfully ... The available categories are:\n");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Failed to store user info");
+        }
+    }
+       
+    /* Behaviour for handling requests from QuizmasterAgent. It calls another function called 'sendCategories()' to fetch the 
+    categories from the databse. */
+    
     private class HandleQuizmasterRequestsBehaviour extends CyclicBehaviour {
         public void action() {
             ACLMessage msg = receive();
@@ -41,6 +91,11 @@ public class Database extends Agent {
         }
     }
 
+    /* 
+    This function runs the query to fetch the categories from the database. These fetched categories are then sent to the 
+    interface agent for category selection. It runs exceptions if the entered command to fetch the categories is infeasible.
+    */
+    
     private void sendCategories() {
         String sql = "SELECT id, name FROM categories ORDER BY id";
         try (Connection conn = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
@@ -61,7 +116,12 @@ public class Database extends Agent {
         }
     }
 
-    //new
+     /* 
+        Behaviour for handling requests from QuizmasterAgent.
+        This behaviour runs a function called 'sendQuestionList(category)'. This function receives the desired category and handles
+        retrieving the questions of the desired category from the database.
+     */
+    
     private class HandleRetrievingQuestionsBehaviour extends CyclicBehaviour {
         public void action() {
             ACLMessage msg = receive();
@@ -76,6 +136,12 @@ public class Database extends Agent {
         }
     }
 
+    /* 
+    This function runs the query to fetch the questions for the desired category from the database. These fetched questions are 
+    then sent to the QuestionInterface agent for further processing. It runs an exception if the questions could not be fetched 
+    for invalid command.
+    */
+    
     private void sendQuestionList(String category) {
         String sql = "SELECT q.id as questionID, q.text as questionText, o.text as optionText "
                 + "FROM questions q "
@@ -125,36 +191,5 @@ public class Database extends Agent {
         msg.addReceiver(new AID(receiverName, AID.ISLOCALNAME));
         msg.setContent(content);
         send(msg);
-    }
-
-    // New Behaviour to handle UserInfo messages
-    private class HandleUserInfoBehaviour extends CyclicBehaviour {
-        public void action() {
-            ACLMessage msg = receive();
-            if (msg != null && msg.getOntology().equals("UserInfo")) {
-                UserInfo userInfo = gson.fromJson(msg.getContent(), UserInfo.class);
-                storeUserInfoInDatabase(userInfo);
-            } else {
-                block();
-            }
-        }
-    }
-
-    private void storeUserInfoInDatabase(UserInfo userInfo) {
-        String sql = "INSERT INTO users (email, password, username) VALUES (?, ?, ?)";
-        try (Connection conn = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-            pstmt.setString(1, userInfo.getEmail());
-            pstmt.setString(2, userInfo.getPassword());
-            pstmt.setString(3, userInfo.getUsername()); // Consider hashing passwords
-
-            pstmt.executeUpdate();
-            System.out.println("\nUser info stored successfully ... The available categories are:\n");
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Failed to store user info");
-        }
     }
 }
