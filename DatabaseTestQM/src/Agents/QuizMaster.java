57d0a3faf1fe5c188b76da8ee8f480df1736008e
Raisa Mehjabin Azni <150555460+raisa-azni@users.noreply.github.com>
2023-12-09 06:01:48 +0600

    Update QuizMaster.java

--- DatabaseTestQM/src/Agents/QuizMaster.java
+++ DatabaseTestQM/src/Agents/QuizMaster.java
@@ -1,3 +1,8 @@
+/*    
+The QuizMaster agent acts as a middle bridge to handle commands sent between the agents. This agent's implementation is more 
+beneficial in real-life scenarios where we may have a large number of agents working on a project.
+*/
+
 package Agents;
 
 import jade.core.AID;
@@ -8,6 +13,7 @@ import jade.lang.acl.ACLMessage;
 public class QuizMaster extends Agent {
     private MessageHandler messageHandler;
 
+    // This is the setup() function which holds the 'agent has been started' comment and the implemented behaviours in this agent.
     @Override
     protected void setup() {
         System.out.println(getLocalName() + " is ready.");
@@ -16,7 +22,13 @@ public class QuizMaster extends Agent {
         addBehaviour(new ReceiveCategorySelectionBehaviour());
     }
 
-    // Behaviour for initiating the quiz
+    /* 
+    Behaviour for initiating the quiz. 
+    It sends the command 'fetch the available categories from the database' to the 
+    database agent. After getting this command, the database agent will fetch the available categories from the database.
+    This command basically starts the quiz process.
+    */
+    
     private class InitiateQuizBehaviour extends CyclicBehaviour {
         public void action() {
             ACLMessage msg = receive();
@@ -29,7 +41,12 @@ public class QuizMaster extends Agent {
         }
     }
 
-    // New Behaviour for receiving category selection from Interface agent
+     /* 
+       Behaviour for receiving category selection from the interface agent.
+       This behavior receives the 'desired category entered by the user' from the interface agent and sends it to the database
+       agent. The database agent will then receive this category serial number and generate the questions for that particular category.
+    */
+    
     private class ReceiveCategorySelectionBehaviour extends CyclicBehaviour {
         public void action() {
             ACLMessage msg = receive();
